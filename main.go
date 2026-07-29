440d30c93e866a9a16c76bd289c090bcbc4778ea
difyz <15564773+difyz@user.noreply.gitee.com>
2026-07-29 22:34:54 +0800

    update:切换到cobra框架

--- main.go
+++ main.go
@@ -1,29 +1,10 @@
 package main
 
-import (
-	"log"
-	"os"
-
-	"github.com/zolagz/ytb2bili-go/internal/config"
-	"github.com/zolagz/ytb2bili-go/internal/command"
-)
+import "github.com/zolagz/ytb2bili-go/internal/cmd"
 
 var Version = "dev"
 
-// pip install "yt-dlp[default,curl-cffi]"
-
 func main() {
-	// 优先加载 config.yaml，不存在则使用默认值
-	cfg, err := config.LoadYAML("config.yaml")
-	if err != nil {
-		cfg = config.Default()
-		cfg.Init()
-	}
-
-	app := command.NewApp(cfg)
-	app.Version = Version
-
-	if err := app.Run(os.Args); err != nil {
-		log.Fatalf("❌ %v", err)
-	}
+	cmd.Version = Version
+	cmd.Execute()
 }
